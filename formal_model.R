f9dd7db6d689a0bfcdfbbfbb9e4d2b39698116eb
yourname <yourname@email.com>
2026-09-16 14:13:42 +0200

    update 2026-09-16 14:13:42.053429

--- formal_model.R
+++ formal_model.R
@@ -1,13 +1,13 @@
 # Set random seed
-set.seed(1813010875)
+set.seed(752072201)
 # Set simulation parameters
 n <- 100
 # Simulate exogenous nodes
 external_event <- rnorm(n = n)
 needs <- rnorm(n = n)
 # Simulate endogenous nodes
-intrinsic_motivation <- 0.27 * needs + rnorm(n = n)
-wellbeing <- 0.49 * needs - 0.01 * intrinsic_motivation + rnorm(n = n)
+intrinsic_motivation <- 0.12 * needs - 0.07 * external_event + rnorm(n = n)
+wellbeing <- 0.15 * intrinsic_motivation + 0.38 * needs + rnorm(n = n)
 df <- data.frame(
 external_event = external_event,
 intrinsic_motivation = intrinsic_motivation,
